07c10648dc666a181bbc70b6cc1ac232ee922555
praveenvjpm <praveenvjpm@gmail.com>
2020-03-02 14:50:30 +0530

    DAO Added

--- src/main/java/io/java/praveen/dao/Data.java
+++ src/main/java/io/java/praveen/dao/Data.java
@@ -2,4 +2,41 @@ package io.java.praveen.dao;
 
 public class Data {
 
+	private String state;
+	private String country;
+	private String corordinates;
+	private long number;
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getCorordinates() {
+		return corordinates;
+	}
+
+	public void setCorordinates(String corordinates) {
+		this.corordinates = corordinates;
+	}
+
+	public long getNumber() {
+		return number;
+	}
+
+	public void setNumber(long number) {
+		this.number = number;
+	}
+
 }
